eeef0f50e46d76d3a64fcf4124887eedb7df7b77
diozenio <d.dionisiobsl@gmail.com>
2024-03-08 18:34:57 -0300

    feat(components): Add Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,18 @@
+interface InputProps {
+  placeholder?: string;
+}
+
+function Input(props: InputProps) {
+  const { placeholder } = props;
+
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      autoFocus
+      className="w-[540px] bg-zinc-800 border-2 border-zinc-950 text-zinc-100 p-4 rounded-lg focus:outline-none placeholder-zinc-600 text-base"
+    />
+  );
+}
+
+export { Input };
