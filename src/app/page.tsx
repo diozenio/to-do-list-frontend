f85b82be09591cba4876b3c68fdfad25ba8521a5
diozenio <d.dionisiobsl@gmail.com>
2024-03-11 20:48:26 -0300

    fix(tasks): Fix TaskList uncomplete height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TasksList from "@/components/tasks/TasksList";
 
 export default function Home() {
   return (
-    <main className="bg-zinc-900 flex flex-1 flex-col">
+    <main className="bg-zinc-900 flex flex-col h-full">
       <header className="w-full h-72 bg-zinc-950 relative flex items-center justify-center">
         <h1 className="font-black text-rose-600 text-[40px]">
           To Do <span className="text-emerald-400">List</span>
--- src/components/tasks/TasksList.tsx
+++ src/components/tasks/TasksList.tsx
@@ -6,7 +6,7 @@ import { NotepadTextIcon } from "@/assets/icons";
 
 function EmptyTasksList() {
   return (
-    <div className="flex flex-col justify-center items-center text-zinc-600 py-8">
+    <div className="text-zinc-600 flex flex-1 flex-col justify-center items-center">
       <NotepadTextIcon size={64} />
       <h3 className="font-bold mt-4">Você ainda não tem tarefas cadastradas</h3>
       <p> Crie tarefas e organize seus itens a fazer</p>
@@ -20,7 +20,7 @@ interface TasksListProps {
 
 function TasksList({ tasks }: TasksListProps) {
   return (
-    <section className="pt-20 w-[700px] 2xl:w-[860px] px-8 max-w-full mx-auto pb-8">
+    <section className="pt-20 w-[700px] 2xl:w-[860px] px-8 max-w-full mx-auto pb-8 h-full flex flex-col">
       <div className="w-full flex flex-row justify-between items-center mb-8">
         <TasksInfo
           label="Tarefas criadas"
@@ -28,15 +28,16 @@ function TasksList({ tasks }: TasksListProps) {
         />
         <TasksInfo label="Concluídas" counter={getCompletedTasksCount(tasks)} />
       </div>
-      <div className="flex flex-col gap-3">
-        {tasks?.length ? (
-          tasks?.map((task) => {
+
+      {tasks?.length ? (
+        <div className="flex flex-col gap-3">
+          {tasks?.map((task) => {
             return <TaskCard key={task.id} task={task} />;
-          })
-        ) : (
-          <EmptyTasksList />
-        )}
-      </div>
+          })}
+        </div>
+      ) : (
+        <EmptyTasksList />
+      )}
     </section>
   );
 }
